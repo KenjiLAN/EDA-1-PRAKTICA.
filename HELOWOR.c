373e1238a47a2dc43b8df74a612a7bdc9657a5d4
Kenji_LAN <alanjuarez6582@gmail.com>
2025-08-26 00:43:39 -0600

    AGREGAR HELLOWORLD

---
+++ HELOWOR.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf(" +------------------------------+\n");
+    printf(" ¦     Hola mundo en C!!!!      ¦\n");
+    printf(" ¦  Espera Realmente alguien    ¦\n");
+    printf(" ¦     Esta leyendo esto...??   ¦\n");
+    printf(" +------------------------------+\n");
+    return 0;
+}
